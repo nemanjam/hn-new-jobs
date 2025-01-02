dffc47d223865e9cc0e1fc5368be3050157bd3ed
nemanjam <nemanja.mitic.elfak@hotmail.com>
2025-01-02 22:24:45 +0100

    remove import

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,13 @@
 import { NextResponse } from 'next/server';
 
-import logger from '@/libs/winston';
-
 import type { NextRequest } from 'next/server';
 
 export const middleware = (request: NextRequest) => {
   const userAgentString = request.headers.get('user-agent') ?? '';
 
   if (userAgentString.includes('Googlebot'))
-    logger.info(
-      `Googlebot detected, pathname: ${request.nextUrl.pathname}, userAgentString: ${userAgentString}`
+    console.log(
+      `Googlebot detected: ${new Date().toISOString()}, pathname: ${request.nextUrl.pathname}, userAgentString: ${userAgentString}`
     );
 
   return NextResponse.next();
